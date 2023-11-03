9ceafa38ebdefb7c3632ef34e7a3b55969c80963
Evan <stevanuspangau50@gmail.com>
2023-11-03 10:52:07 +0700

    variable

---
+++ dart-basics/02_variables.dart
@@ -0,0 +1,9 @@
+void main() {
+  int outputInt = 2; // initialization
+
+  print('outputInt: $outputInt.');
+
+  late int lateOutputInt; // declaration
+  lateOutputInt = 42; // allocation
+  print('lateOutputInt: $lateOutputInt');
+}
