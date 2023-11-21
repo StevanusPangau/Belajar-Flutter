56c6064e87a6a1237c898632a8918d23c4f12db2
Evan <stevanuspangau50@gmail.com>
2023-11-21 16:09:16 +0700

    null safety

---
+++ dart-basics/21_null-safety.dart
@@ -0,0 +1,20 @@
+void main() {
+  int nonNullable = 10; // this value can't be null
+  // nonNullable = null; // error
+
+  int? nullable = null; // this value can be null
+  // nullable = 10; // no error
+
+  // String description; // not initialized
+  late String description;
+  description = 'description'; // initialized
+
+  int value =
+      nullable!; // non null assertion operator, ensure dart this is not null by time
+
+  int value2 = nullable ?? 1; // 1 is default value if nullable is null
+
+  String? nullableString = null;
+  int stringLength = nullableString?.length ??
+      0; // 0 is default value if nullableString is null
+}
