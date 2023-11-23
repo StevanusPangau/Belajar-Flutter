4e907ae5b7b4f78f711d3b2692621fbf6d38a10c
Evan <stevanuspangau50@gmail.com>
2023-11-23 15:00:15 +0700

    Widget - container & Text

--- 2_flutter/lib/main.dart
+++ 2_flutter/lib/main.dart
@@ -10,14 +10,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(appBarTheme: const AppBarTheme(color: Colors.amber)),
+      theme: ThemeData(appBarTheme: const AppBarTheme(color: Colors.blue)),
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
           title: const Text('Flutter Basics'),
         ),
-        body: const Center(
-          child: Text('Hello World!!!'),
+        body: Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const Center(
+              child: Text(
+                'Hello World!!!',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 20,
+                  // fontStyle: FontStyle.italic,
+                  // fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () => debugPrint('FAB pressed'),
