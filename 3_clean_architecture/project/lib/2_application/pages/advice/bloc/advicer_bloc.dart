65d638e6acbd4e633b13e1d62037d2a0613e51f7
Evan <stevanuspangau50@gmail.com>
2024-01-25 23:00:01 +0700

    setting for the bloc

---
+++ 3_clean_architecture/project/lib/2_application/pages/advice/bloc/advicer_bloc.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'advicer_event.dart';
+part 'advicer_state.dart';
+
+class AdvicerBloc extends Bloc<AdvicerEvent, AdvicerState> {
+  AdvicerBloc() : super(AdvicerInitial()) {
+    on<AdviceRequestEvent>((event, emit) async {
+      emit(AdvicerStateLoading());
+      // execute business logic
+      debugPrint("Fake get advice triggered");
+      await Future.delayed(Duration(seconds: 3), () {});
+      debugPrint("got advice");
+      emit(AdvicerStateLoaded(advice: "Fake advice to test bloc"));
+      // emit(AdvicerStateError(message: "Fake error to test bloc"));
+    });
+  }
+}
---
+++ 3_clean_architecture/project/lib/2_application/pages/advice/bloc/advicer_state.dart
@@ -0,0 +1,18 @@
+part of 'advicer_bloc.dart';
+
+@immutable
+sealed class AdvicerState {}
+
+final class AdvicerInitial extends AdvicerState {}
+
+class AdvicerStateLoading extends AdvicerState {}
+
+class AdvicerStateLoaded extends AdvicerState {
+  final String advice;
+  AdvicerStateLoaded({required this.advice});
+}
+
+class AdvicerStateError extends AdvicerState {
+  final String message;
+  AdvicerStateError({required this.message});
+}
